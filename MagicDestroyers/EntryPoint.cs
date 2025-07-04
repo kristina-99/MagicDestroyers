8cc44e767e68dec3ee3c81b2992740162e2f22ac
kristina-99 <kristina_atan@abv.bg>
2025-07-04 16:18:49 +0300

    * created a new List of Characters in the EntryPoint class * divided the characters between two teams using a foreach loop

--- MagicDestroyers/EntryPoint.cs
+++ MagicDestroyers/EntryPoint.cs
@@ -13,24 +13,34 @@ namespace MagicDestroyers
     {
         static void Main()
         {
-            Character warrior = new Warrior();
-            Character knight = new Knight();
-            Character assassin = new Assassin();
-            Character mage = new Mage();
-            Character necromancer = new Necromancer();
-            Character druid = new Druid();
+            List<Character> characters = new List<Character>()
+            {
+                new Warrior(),
+                new Warrior(),
+                new Warrior(),
+                //new Knight(),
+                //new Assassin(),
+                new Mage(),
+                new Mage(),
+                new Mage()
+                //new Necromancer(),
+                //new Druid()
+            };
 
-            List<Character> meleeTeam = new List<Character>();
-            List<Character> spellTeam = new List<Character>();
+            List<Melee> meleeTeam = new List<Melee>();
+            List<Spellcaster> spellTeam = new List<Spellcaster>();
 
-            meleeTeam.Add(warrior);
-            meleeTeam.Add(knight);
-            meleeTeam.Add(assassin);
-
-            spellTeam.Add(mage);
-            spellTeam.Add(necromancer);
-            spellTeam.Add(druid);
-
+            foreach (var character in characters)
+            {
+                if (character is Melee)
+                {
+                    meleeTeam.Add((Melee)character);
+                }
+                else if (character is Spellcaster)
+                {
+                    spellTeam.Add((Spellcaster)character);
+                }
+            }
         }
     }
 }
