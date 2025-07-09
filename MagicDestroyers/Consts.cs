d87fb7ed9cf9bcd66687be2eba33ef8e84f7d3a6
kristina-99 <kristina_atan@abv.bg>
2025-07-09 13:48:45 +0300

    * added constants for all character types to the Consts class

--- MagicDestroyers/Consts.cs
+++ MagicDestroyers/Consts.cs
@@ -18,13 +18,49 @@ namespace MagicDestroyers
             public const Factions FACTION = Factions.Melee;
         }
 
+        public static class Assasin
+        {
+            public const string NAME = "Sam";
+            public const int LEVEL = 1;
+            public const int ABILITY_POINTS = 10;
+            public const int HEALTH_POINTS = 100;
+            public const Factions FACTION = Factions.Melee;
+        }
+
+        public static class Knight
+        {
+            public const string NAME = "Arthur";
+            public const int LEVEL = 1;
+            public const int ABILITY_POINTS = 10;
+            public const int HEALTH_POINTS = 100;
+            public const Factions FACTION = Factions.Melee;
+        }
+
         public static class Mage
         {
             public const string NAME = "Merlin";
             public const int LEVEL = 1;
             public const int MANA_POINTS = 10;
             public const int HEALTH_POINTS = 100;
-            public const Factions FACTION = Factions.Melee;
+            public const Factions FACTION = Factions.Spellcaster;
+        }
+
+        public static class Druid
+        {
+            public const string NAME = "River";
+            public const int LEVEL = 1;
+            public const int MANA_POINTS = 10;
+            public const int HEALTH_POINTS = 100;
+            public const Factions FACTION = Factions.Spellcaster;
+        }
+
+        public static class Necromancer
+        {
+            public const string NAME = "Nec";
+            public const int LEVEL = 1;
+            public const int MANA_POINTS = 10;
+            public const int HEALTH_POINTS = 100;
+            public const Factions FACTION = Factions.Spellcaster;
         }
     }
 }
